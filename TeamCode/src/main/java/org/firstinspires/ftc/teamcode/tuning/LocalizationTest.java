67f2d3f90a0daf4699856a10e2792c9c8e89e165
ftc30316 <ftc30316@gmail.com>
2025-09-08 19:36:17 -0700

    simulated limelight

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/LocalizationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/LocalizationTest.java
@@ -7,7 +7,9 @@ import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.PoseVelocity2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.teamcode.Drawing;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.TankDrive;
@@ -16,10 +18,10 @@ public class LocalizationTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        Servo servo1 = hardwareMap.get(Servo.class, "servo1");
 
         if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
             MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
-
             waitForStart();
 
             while (opModeIsActive()) {
@@ -32,18 +34,45 @@ public class LocalizationTest extends LinearOpMode {
                 ));
 
                 drive.updatePoseEstimate();
+//LLResult result = limelight.getLatestResult();
+//if (result != null && result.isValid()) {
+//    double tx = result.getTx(); // How far left or right the target is (degrees)
+//    double ty = result.getTy(); // How far up or down the target is (degrees)
+//    double ta = result.getTa(); // How big the target looks (0%-100% of the image)
+//
+//    telemetry.addData("Target X", tx);
+//    telemetry.addData("Target Y", ty);
+//    telemetry.addData("Target Area", ta);
+//} else {
+//    telemetry.addData("Limelight", "No Targets");
+//}
+                //gives random error between -1 and 1; supposed to simulate limelight
+                double error = (Math.random() * 2) - 1.0;
+                //moves servo to counteract error
+                servo1.setPosition(error * -1);
 
                 Pose2d pose = drive.localizer.getPose();
+                
+                //when goal is positioned at 60,60, we get relative distance for x and y values
+                double xDistanceFromGoal = Math.abs(pose.position.x -  60);
+                double yDistanceFromGoal = Math.abs(pose.position.y -  60);
+
                 telemetry.addData("x", pose.position.x);
                 telemetry.addData("y", pose.position.y);
                 telemetry.addData("heading (deg)", Math.toDegrees(pose.heading.toDouble()));
+                telemetry.addData("distance from goal:" , String.valueOf(xDistanceFromGoal) + "," + String.valueOf(yDistanceFromGoal));
+                telemetry.addData("error: " , String.valueOf(error));
                 telemetry.update();
+                sleep(1000);
 
                 TelemetryPacket packet = new TelemetryPacket();
                 packet.fieldOverlay().setStroke("#3F51B5");
                 Drawing.drawRobot(packet.fieldOverlay(), pose);
                 FtcDashboard.getInstance().sendTelemetryPacket(packet);
             }
+
+
+
         } else if (TuningOpModes.DRIVE_CLASS.equals(TankDrive.class)) {
             TankDrive drive = new TankDrive(hardwareMap, new Pose2d(0, 0, 0));
 
@@ -61,6 +90,8 @@ public class LocalizationTest extends LinearOpMode {
                 drive.updatePoseEstimate();
 
                 Pose2d pose = drive.localizer.getPose();
+
+
                 telemetry.addData("x", pose.position.x);
                 telemetry.addData("y", pose.position.y);
                 telemetry.addData("heading (deg)", Math.toDegrees(pose.heading.toDouble()));
