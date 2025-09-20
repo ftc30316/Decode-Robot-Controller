3630b858f915f59f798d79c0e2664f7d4426a66b
ftc30316 <ftc30316@gmail.com>
2025-09-19 17:48:12 -0700

    Fixed position tracking on DriveTurretTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/DriveTurretTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/DriveTurretTest.java
@@ -25,47 +25,62 @@ public class DriveTurretTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         DcMotorEx leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
+//        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
 
         if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
-            MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+            //MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
 
             // Reset the encoder during initialization
             leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
             waitForStart();
+            double ticksPerRev = leftBack.getMotorType().getTicksPerRev();
+            //leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            TelemetryPacket packet = new TelemetryPacket();
+            packet.put("velocity", leftBack.getVelocity());
+            packet.put("position", leftBack.getCurrentPosition());
+            packet.put("is at target", !leftBack.isBusy());
+            packet.put("ticks per rev", ticksPerRev);
+            FtcDashboard.getInstance().sendTelemetryPacket(packet);
+
+            sleep(5000);
+
+            //Set the amount of ticks to move
+            leftBack.setTargetPosition(30);
+
+            // Switch to RUN_TO_POSITION mode
+            leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // Start the motor moving by setting the max velocity to ___ ticks per second
+            leftBack.setVelocity(10);
+
+
 
 
             while (opModeIsActive()) {
-                drive.setDrivePowers(new PoseVelocity2d(
-                        new Vector2d(
-                                -gamepad1.left_stick_y,
-                                -gamepad1.left_stick_x
-                        ),
-                        -gamepad1.right_stick_x
-                ));
+//                drive.setDrivePowers(new PoseVelocity2d(
+//                        new Vector2d(
+//                                -gamepad1.left_stick_y,
+//                                -gamepad1.left_stick_x
+//                        ),
+//                        -gamepad1.right_stick_x
+//                ));
+//
+//                drive.updatePoseEstimate();
+//
+//                Pose2d pose = drive.localizer.getPose();
 
-                drive.updatePoseEstimate();
 
-                Pose2d pose = drive.localizer.getPose();
 
-                leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-                //Set the amount of ticks to move
-                leftBack.setTargetPosition(250);
-
-                // Switch to RUN_TO_POSITION mode
-                leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-                // Start the motor moving by setting the max velocity to ___ ticks per second
-                leftBack.setVelocity(1000);
-
-                TelemetryPacket packet = new TelemetryPacket();
+                packet = new TelemetryPacket();
                 packet.put("velocity", leftBack.getVelocity());
                 packet.put("position", leftBack.getCurrentPosition());
                 packet.put("is at target", !leftBack.isBusy());
+                packet.put("ticks per rev", ticksPerRev);
                 FtcDashboard.getInstance().sendTelemetryPacket(packet);
                 telemetry.update();
-                sleep(100);
+                sleep(50);
             }
         }
         else {
