75f9a956b9f05316adb71dc5e4f7b319e6d75578
ftc30316 <ftc30316@gmail.com>
2025-09-26 18:19:47 -0700

    Added state machines and states you can be in

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/ArtifactSorterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/ArtifactSorterTest.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp
 
@@ -16,6 +17,19 @@ public class ArtifactSorterTest extends LinearOpMode {
     private NormalizedColorSensor artifactSlot2;
     private NormalizedColorSensor artifactSlot3;
     private String nextColorArtifact = "purple";
+
+    enum State {
+        DETECTING,
+        HOLDING,
+        FLICKING,
+        RESETTING
+    }
+
+    State sorterState = State.DETECTING;
+    ElapsedTime flickTimer = new ElapsedTime();  // Timer to track time in each state
+
+    private final double FLICK_DISTANCE = 0.5;
+    private final double TRAVEL_TIME = 3.0;
 
     @Override
     public void runOpMode() {
@@ -47,46 +61,74 @@ public class ArtifactSorterTest extends LinearOpMode {
             telemetry.addData("Green1", color1.green);
             telemetry.addData("Blue1", color1.blue);
 
-            telemetry.addData("Red2", color2.red);
-            telemetry.addData("Green2", color2.green);
-            telemetry.addData("Blue2", color2.blue);
-
-            telemetry.addData("Red3", color3.red);
-            telemetry.addData("Green3", color3.green);
-            telemetry.addData("Blue3", color3.blue);
-
-
-            String sensor1color = colorDetection(color1);
-            String sensor2color = colorDetection(color2);
-            String sensor3color = colorDetection(color3);
+//            telemetry.addData("Red2", color2.red);
+//            telemetry.addData("Green2", color2.green);
+//            telemetry.addData("Blue2", color2.blue);
+//
+//            telemetry.addData("Red3", color3.red);
+//            telemetry.addData("Green3", color3.green);
+//            telemetry.addData("Blue3", color3.blue);
 
             telemetry.update();
+
+//            String sensor2color = colorDetection(color2);
+//            String sensor3color = colorDetection(color3);
 
             TelemetryPacket packet = new TelemetryPacket();
             packet.put("Red1", color1.red);
             packet.put("Green1", color1.green);
             packet.put("Blue1", color1.blue);
 
-            packet.put("Red2", color2.red);
-            packet.put("Green2", color2.green);
-            packet.put("Blue2", color2.blue);
-
-            packet.put("Red3", color3.red);
-            packet.put("Green3", color3.green);
-            packet.put("Blue3", color3.blue);
+//            packet.put("Red2", color2.red);
+//            packet.put("Green2", color2.green);
+//            packet.put("Blue2", color2.blue);
+//
+//            packet.put("Red3", color3.red);
+//            packet.put("Green3", color3.green);
+//            packet.put("Blue3", color3.blue);
             FtcDashboard.getInstance().sendTelemetryPacket(packet);
 
-            if (sensor1color != null && sensor1color.equals(nextColorArtifact)) {
-                servo1.setPosition(1);
-                nextColorArtifact = "green";
+//            if (sensor1color != null && sensor1color.equals(nextColorArtifact)) {
+//                servo1.setPosition(1);
+//                nextColorArtifact = "green";
+//
+//            } else if (sensor2color != null && sensor2color.equals(nextColorArtifact)) {
+//                servo2.setPosition(1);
+//                nextColorArtifact = "green";
+//            } else if (sensor3color != null && sensor3color.equals(nextColorArtifact)) {
+//                servo3.setPosition(1);
+//                nextColorArtifact = "green";
+//            }
 
-            } else if (sensor2color != null && sensor2color.equals(nextColorArtifact)) {
-                servo2.setPosition(1);
-                nextColorArtifact = "green";
-            } else if (sensor3color != null && sensor3color.equals(nextColorArtifact)) {
-                servo3.setPosition(1);
-                nextColorArtifact = "green";
+            telemetry.addData("Current State ", sorterState);
+            switch (sorterState) {
+                case DETECTING:
+                    String sensor1color = colorDetection(color1);
+                    telemetry.addLine("The detecting color is "+ sensor1color);
+                    if (sensor1color != null) {
+                        sorterState = State.HOLDING;
+                    }
+                    break;
+                case HOLDING:
+                    String sensor1colorHolding = colorDetection(color1);
+                    telemetry.addLine("The holding color is "+ sensor1colorHolding);
+                    if (sensor1colorHolding != null && sensor1colorHolding.equals(nextColorArtifact) && gamepad1.right_trigger == 1.0) {
+                        sorterState = State.FLICKING;
+                    }
+                    break;
+                case FLICKING:
+                    flickTimer.reset();
+                    servo1.setPosition(FLICK_DISTANCE);
+                    sorterState = State.RESETTING;
+                    break;
+                case RESETTING:
+                    if (flickTimer.seconds() > TRAVEL_TIME) {
+                        servo1.setPosition(0.0);
+                        sorterState = State.DETECTING;
+                    }
+                    break;
             }
+
         }
     }
 
