028af72eb8d5efcb8dbd0f6603797a825d6d52a1
ftc30316 <ftc30316@gmail.com>
2026-01-11 13:12:57 -0800

    Updated PID test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/evergreendynamics/testers/PIDFControllerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/evergreendynamics/testers/PIDFControllerTest.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 /**
@@ -22,7 +23,8 @@ import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 public class PIDFControllerTest extends LinearOpMode {
 
     // our DC motor.
-    DcMotorEx turretMotor;
+    DcMotorEx leftFlywheel;
+    DcMotorEx rightFlywheel;
 
     public static double NEW_P = 10.0;
     public static double NEW_I = 3.0;
@@ -33,13 +35,16 @@ public class PIDFControllerTest extends LinearOpMode {
         // get reference to DC motor.
         // since we are using the Control Hub or Expansion Hub,
         // cast this motor to a DcMotorEx object.
-        turretMotor = (DcMotorEx)hardwareMap.get(DcMotor.class, "leftFlywheel");
+        leftFlywheel = (DcMotorEx)hardwareMap.get(DcMotor.class, "leftFlywheel");
+        rightFlywheel = (DcMotorEx)hardwareMap.get(DcMotor.class, "rightFlywheel");
 
+        leftFlywheel.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFlywheel.setDirection(DcMotorSimple.Direction.FORWARD);
         // wait for start command.
         waitForStart();
 
         // get the PID coefficients for the RUN_USING_ENCODER  modes.
-        PIDFCoefficients pidfOrig = turretMotor.getPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER);
+        PIDFCoefficients pidfOrig = leftFlywheel.getPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // change coefficients using methods included with DcMotorEx class.
         PIDFCoefficients pidfModified = new PIDFCoefficients(NEW_P, NEW_I, NEW_D, NEW_F);
@@ -59,16 +64,19 @@ public class PIDFControllerTest extends LinearOpMode {
                     pidfModified.p, pidfModified.i, pidfModified.d, pidfModified.f);
 
             telemetry.addData("Target Velocity", targetVelocity);
-            telemetry.addData("Current Velocity", turretMotor.getVelocity());
+            telemetry.addData("Current Velocity", leftFlywheel.getVelocity());
 
             TelemetryPacket packet = new TelemetryPacket();
             packet.put("Target Velocity", targetVelocity);
-            packet.put("Current Velocity", turretMotor.getVelocity());
+            packet.put("Left Current Velocity", leftFlywheel.getVelocity());
+            packet.put("Right Current Velocity", rightFlywheel.getVelocity());
+
             FtcDashboard.getInstance().sendTelemetryPacket(packet);
 
             if (gamepad1.crossWasPressed()) {
                 pidfModified = new PIDFCoefficients(NEW_P, NEW_I, NEW_D, NEW_F);
-                turretMotor.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfModified);
+                leftFlywheel.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfModified);
+                rightFlywheel.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfModified);
             }
 
             if (gamepad1.dpadDownWasPressed()) {
@@ -79,7 +87,8 @@ public class PIDFControllerTest extends LinearOpMode {
                 targetVelocity = 1500;
             }
 
-            turretMotor.setVelocity(targetVelocity);
+            leftFlywheel.setVelocity(targetVelocity);
+            rightFlywheel.setVelocity(targetVelocity);
 
             telemetry.update();
         }
