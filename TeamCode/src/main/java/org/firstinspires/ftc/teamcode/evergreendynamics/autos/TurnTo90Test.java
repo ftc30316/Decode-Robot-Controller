90ee72f5b2af62cb8ef3f93b2a34a22e5a346d5c
ftc30316 <ftc30316@gmail.com>
2026-07-22 18:34:25 -0700

    created auto to test shortcut

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/evergreendynamics/autos/TurnTo90Test.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode.evergreendynamics.autos;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.ProfileAccelConstraint;
+import com.acmerobotics.roadrunner.TranslationalVelConstraint;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.evergreendynamics.robot.DouglasFIRST;
+import org.firstinspires.ftc.teamcode.evergreendynamics.robot.InputValues;
+import org.firstinspires.ftc.teamcode.evergreendynamics.robot.Turret;
+
+@Autonomous (group = "Evergreen Autos")
+public class TurnTo90Test extends LinearOpMode {
+    public DouglasFIRST douglasFIRST;
+
+    @Override
+
+    public void runOpMode() {
+
+        try {
+            telemetry.addLine("Running Op Mode");
+            Pose2d beginPose = new Pose2d(0, 0, Math.toRadians(0)); // changed from 270 to -90
+            float turretStartHeading = -90;
+            double robotStartHeading = Math.toDegrees(beginPose.heading.toDouble());
+
+            this.douglasFIRST = new DouglasFIRST(hardwareMap, gamepad1, gamepad2, telemetry, InputValues.Alliance.BLUE, beginPose, DouglasFIRST.DriveMode.ROBOT_CENTRIC, Turret.TurretVelocityMode.AUTO);
+
+            waitForStart();
+
+            //Creates background thread
+            douglasFIRST.start(robotStartHeading, turretStartHeading, true);
+
+            // strafes to upper launch zone
+            Actions.runBlocking(douglasFIRST.getActionBuilder(beginPose).setTangent(0)
+                            .turnTo(Math.toRadians(1))
+                            .waitSeconds(0.5)
+                            .turnTo(Math.toRadians(douglasFIRST.goToNearest90(1)))
+                            .waitSeconds(0.5)
+                            .turnTo(Math.toRadians(46))
+                            .turnTo(Math.toRadians(douglasFIRST.goToNearest90(46)))
+                            .waitSeconds(0.5)
+                            .turnTo(Math.toRadians(359))
+                            .turnTo(Math.toRadians(douglasFIRST.goToNearest90(359)))
+                            .waitSeconds(0.5)
+                            .build());
+
+            douglasFIRST.savePose();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            douglasFIRST.turret.stopTurretBackgroundThread();
+        }
+    }
+
+}
