c5346d2b159ee79f32d84f66564d098dbda9b2f8
Evergreen Dynamics FTC 30316 <ftc30316@gmail.com>
2026-09-13 10:31:34 -0700

    Flywheel single test 30 hours

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/evergreendynamics/testers/FlywheelSingleTest.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.evergreendynamics.testers;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+
+public class FlywheelSingleTest extends LinearOpMode {
+    private DcMotor flywheel1;
+
+    @Override
+    public void runOpMode() {
+        flywheel1 = hardwareMap.get(DcMotor.class, "flywheel1");
+
+        flywheel1.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            double power = -gamepad1.left_stick_y * 0.6;
+
+            flywheel1.setPower(power);
+
+            telemetry.addData("Power:", power);
+            telemetry.update();
+
+            TelemetryPacket packet = new TelemetryPacket();
+            packet.put("Power:", power);
+            FtcDashboard.getInstance().sendTelemetryPacket(packet);
+
+            idle();
+        }
+    }
+
+}
