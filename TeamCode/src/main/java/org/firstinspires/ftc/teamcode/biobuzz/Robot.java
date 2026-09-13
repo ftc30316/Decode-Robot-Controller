838292f3f45d8232e4ca9ede0f41df001e2681c6
ftc30316 <ftc30316@gmail.com>
2026-09-13 11:44:33 -0700

    Moves flywheel transfer constantly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/biobuzz/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/biobuzz/Robot.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.biobuzz;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
@@ -15,15 +16,23 @@ public class Robot {
 
     private MecanumDrive mecanumDrive;
     private IntakeComponent intake;
+    private HardwareMapping hardwareMapping;
+    private Config config;
     public Robot (HardwareMap hardwareMap, Gamepad gamepad1, Gamepad gamepad2, Telemetry telemetry, Pose2d beginPose){
-        Config config = new Config();
-        HardwareMapping hardwareMapping = new HardwareMapping(config, hardwareMap);
+        this.config = new Config();
+        this.hardwareMapping = new HardwareMapping(config, hardwareMap);
         Keybinds keybinds = new Keybinds(gamepad1, gamepad2);
         this.mecanumDrive = new MecanumDrive(hardwareMap, keybinds, beginPose);
         this.intake = new IntakeComponent(config, hardwareMapping, keybinds);
     }
 
+    public void init(){
+        DcMotorEx OuttakeFlywheelMotor = this.hardwareMapping.getOuttakeFlywheelMotor();
+        OuttakeFlywheelMotor.setVelocity(config.OuttakeFlywheelMotorDefaultVelocity);
+    }
     public void loop() {
+        DcMotorEx OuttakeFlywheelMotor = this.hardwareMapping.getOuttakeFlywheelMotor();
+
         Pose2d currentPose = getCurrentPose();
 //        telemetry.addData("Robot X", currentPose.position.x);
 //        telemetry.addData("Robot Y", currentPose.position.y);
